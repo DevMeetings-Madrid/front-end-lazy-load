d2dd076bb20d6d1a93a8dd6b1d65af5a85ce3621
Fran Linde Blázquez <franlindebl@gmail.com>
2019-11-28 18:21:06 +0100

    Added preconnect example

---
+++ code/link-preconnect/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let image = document.createElement("img");
+  image.src = "http://placekitten.com/600/600";
+  document.body.appendChild(image);
+});
